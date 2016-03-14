af4bd5f672f15068c5b9c9243df4d5989dd52e91
jflorimo <jflorimo@student.42.fr>
2016-03-14 16:40:59 +0100

    custom cell

---
+++ swifty/MealTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MealTableViewCell.swift
+//  swifty
+//
+//  Created by Jean-christophe FLORIMONT on 3/14/16.
+//  Copyright © 2016 Jean-christophe FLORIMONT. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+}
